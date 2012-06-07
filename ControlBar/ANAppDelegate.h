586377742c4e30bd820259826aee1f26e16940e8
Alex Nichol <alexnichol@comcast.net>
2012-06-06 20:33:15 -0400

    Initial Commit

---
+++ ControlBar/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  ControlBar
+//
+//  Created by Alex Nichol on 6/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ANAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
